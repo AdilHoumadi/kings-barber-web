ac8df0084ed68e7f6ca2e228492b5d0594b66853
AdilHoumadi <adil.houmadi@gmail.com>
2026-01-01 09:24:31 +0000

    Update from https://github.com/AdilHoumadi/kings-barber/commit/f40a8b2b71fc9a7785d0ba7ce6c2ac55184f660e

--- assets/user-D_b0jYVS.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BdQM8Exr.js";const e=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],r=c("chevron-right",e);const o=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],s=c("user",o);export{r as C,s as U};
